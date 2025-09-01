f639440b720c06721357f2b6f2728e5016c0b864
ygordantas <ygord.furtado@gmail.com>
2025-08-31 19:30:05 -0600

    implemented file upload

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,21 +1,34 @@
-import Button from "../../components/Button/Button";
+import { useRef } from "react";
 import useAuthContext from "../../hooks/useAuthContext";
-import eventsServices from "../../services/eventsServices";
+import Button from "../../components/Button/Button";
+import { fileStorage } from "../../firebase";
+import { ref, uploadBytes } from "firebase/storage";
 
 export default function HomePage() {
   const { user } = useAuthContext();
 
+  const fileInputRef: React.RefObject<HTMLInputElement | null> = useRef(null);
+
+  const onSaveFileHandler = async () => {
+    if (fileInputRef.current?.files?.length !== 1) return;
+
+    const file = fileInputRef.current.files[0];
+    const fileRef = ref(fileStorage, file.name);
+    await uploadBytes(fileRef, file);
+  };
+
   return (
     <div>
       Hello {user!.firstName} {user!.lastName}
+      <input ref={fileInputRef} type="file" style={{ display: "none" }} />
       <Button
-        onClick={async () => {
-          const response = await eventsServices.getCustomEvent();
-          console.log(response);
+        onClick={() => {
+          fileInputRef.current?.click();
         }}
       >
-        Load events
+        Upload a file
       </Button>
+      <Button onClick={onSaveFileHandler}>Save File</Button>
     </div>
   );
 }
